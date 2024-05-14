85732411183b4afa90f9f72c4ac2da30d4abfb3f
lewis-kori <lewiskosh@gmail.com>
2024-05-14 10:49:33 +0300

    Update user manager

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/login/managers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/login/managers.py
@@ -1,18 +1,41 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import UserManager
 from django.db import models
 
 
 class CustomUserManager(UserManager):
-    ...
-    # def create_user(self, email=None, password=None, **extra_fields):
-    #     if extra_fields.get("username"):
-    #         return super().create_user(email=email, password=password, **extra_fields)
-    #     return super().create_user(username=email, email=email, password=password, **extra_fields)
+    """Custom manager for the User model."""
 
-    # def create_superuser(self, email=None, password=None, **extra_fields):
-    #     if extra_fields.get("username"):
-    #         return super().create_superuser(email=email, password=password, **extra_fields)
-    #     return super().create_superuser(username=email, email=email, password=password, **extra_fields)
+    def _create_user(self, email: str, password: str | None, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            msg = "The given email must be set"
+            raise ValueError(msg)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email: str, password: str | None = None, **extra_fields):  # type: ignore[override]
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email: str, password: str | None = None, **extra_fields):  # type: ignore[override]
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            msg = "Superuser must have is_staff=True."
+            raise ValueError(msg)
+        if extra_fields.get("is_superuser") is not True:
+            msg = "Superuser must have is_superuser=True."
+            raise ValueError(msg)
+
+        return self._create_user(email, password, **extra_fields)
 
     def get_by_natural_key(self, username):
         return self.get(
