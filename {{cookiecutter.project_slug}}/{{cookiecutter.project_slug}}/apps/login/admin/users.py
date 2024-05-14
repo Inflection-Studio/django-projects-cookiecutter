97db97540e8708b54f060d159cf822f63d4507f1
lewis-kori <lewiskosh@gmail.com>
2024-05-14 08:02:20 +0300

    conditional phone numbers field in admin

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/login/admin/users.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/login/admin/users.py
@@ -17,7 +17,7 @@ class UserAdmin(UserAdmin):
         (_("Personal info"), {"fields": ("first_name", "last_name")}),
         {% else %}
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "phone_number")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", {%- if cookiecutter.use_phone_numbers_field == "y" %} "phone_number" {%- endif %} )}),
         {% endif %}
         (
             _("Permissions"),
