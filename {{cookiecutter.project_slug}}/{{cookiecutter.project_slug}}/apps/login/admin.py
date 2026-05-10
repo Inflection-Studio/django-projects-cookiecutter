81ed51642331e82ebf80ab180ca659f1070c3bdc
tngeene <tngeene27@gmail.com>
2026-05-10 16:45:07 +0300

    remove conflicting user admin folder

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/login/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/apps/login/admin.py
@@ -1,1 +1,57 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    list_display = (
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    ordering = ["id"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
